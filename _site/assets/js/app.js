a69e0cf6d7a20e6a8caebd7f5a55b8420d36eb77
Steve H <stehar14@yahoo.com>
2019-01-17 21:29:23 -0500

    will add jekyll later

---
+++ _site/assets/js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    if (window.pageYOffset >= 101) {
+        $(".navbar").addClass("collapsed")
+    }
+});
+$(function() {
+    var header = $(".navbar");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 101) {
+            header.addClass("collapsed");
+        } else {
+            header.removeClass("collapsed");
+        }
+    });
+});
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      $(".footer-top").addClass("bottom");
+  } else {
+    $(".footer-top").removeClass("bottom");
+  }
+});
+
+(function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 116)
+          }, 300, "linear");
+          return false;
+        }
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#top-nav',
+      offset: 116
+    });
+  
+  })(jQuery); // End of use strict
